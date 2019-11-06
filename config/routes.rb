d553b691dafd8defd1595153af64bf6fd13cee9e
Peter Jang <peter.x.jang@gmail.com>
2019-11-05 18:42:30 -0600

    Add API action to see all contacts (not great without loop)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/first_contact_url" => "contacts#first_contact_method"
+    get "/all_contacts_url" => "contacts#all_contacts_method"
   end
 end
