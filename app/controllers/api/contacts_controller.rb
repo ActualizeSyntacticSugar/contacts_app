7bb910a98eae986b149e40fc2ad1208f9bfc9dcf
Peter Jang <peter.x.jang@gmail.com>
2019-11-14 19:04:05 -0600

    Add searching option for contacts index action

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,6 +1,16 @@
 class Api::ContactsController < ApplicationController
   def index
     @contacts = Contact.all
+
+    if params[:search]
+      @contacts = @contacts.where(
+        "first_name ILIKE ? OR last_name ILIKE ? OR email ILIKE ?",
+        "%#{params[:search]}%",
+        "%#{params[:search]}%",
+        "%#{params[:search]}%",
+      )
+    end
+
     render "index.json.jb"
   end
 
